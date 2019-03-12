3a2ff2bd5cb09469354ad1448eb49ced0c078c8d
root <fr13ndz@protonmail.com>
2019-03-12 15:30:06 +0100

    gradients

--- lib/neural_networks/opt/vanilla_gradesc.py
+++ lib/neural_networks/opt/vanilla_gradesc.py
@@ -3,15 +3,36 @@ import theano.tensor as T
 import numpy as np
 
 def clipped_gradients(gradients, gradient_clipping):
+    """
+    Clipping the gradient to guarantee more variety of directions.
+    :param gradients: Gradients.
+    :param gradient_clipping: Clipping values.
+    :return: Clipped gradient.
+    """
     clipped_grads = [T.clip(g, -gradient_clipping, gradient_clipping)
                      for g in gradients]
     return clipped_grads
 
 def gradient_descent(learning_rate, parameters, gradients):
+    """
+    Vanilla gradient descent.
+    :param learning_rate: Learning rate.
+    :param parameters: Additional parameters.
+    :param gradients: Respective Gradients.
+    :return: Update.
+    """
     updates = [(p, p - learning_rate * g) for p, g in zip(parameters, gradients)]
     return updates
 
 def gradient_descent_momentum(learning_rate, momentum, parameters, gradients):
+    """
+    Vanilla gradient descent with momentum term.
+    :param learning_rate: Learning rate.
+    :param momentum: Momentum.
+    :param parameters: Additional parameters.
+    :param gradients: Gradients.
+    :return: Update.
+    """
     velocities = [theano.shared(np.zeros_like(p.get_value(),
                                               dtype=theano.config.floatX)) for p in parameters]
 
